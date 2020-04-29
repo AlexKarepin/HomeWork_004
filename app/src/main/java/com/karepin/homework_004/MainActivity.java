a2fe4f8c8833ce3511abd13985dca1d9e2d1b8d4
Alex Karepin <akarepin@gmail.com>
2020-04-29 08:53:46 +0300

    Initial commit Task 4

---
+++ app/src/main/java/com/karepin/homework_004/MainActivity.java
@@ -0,0 +1,49 @@
+package com.karepin.homework_004;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.sql.SQLOutput;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView;
+    Random random = new Random(100);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        forth();
+
+        back();
+    }
+    private void forth() {
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                int number = random.nextInt(100);
+                String str = Integer.toString(number);
+                textView.setText(str);
+            }
+        });
+    }
+    private void back() {
+        button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
